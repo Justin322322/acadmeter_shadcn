daa9a199e89d260ece918c1a69b69c0f63d1bd7e
Justin322322 <jmrsibonga@bpsu.edu.ph>
2025-06-01 11:50:49 +0800

    Updated dashboard-chart.tsx

--- src/components/admin/dashboard-chart.tsx
+++ src/components/admin/dashboard-chart.tsx
@@ -120,7 +120,7 @@ export function DashboardChart({
     switch (type) {
       case 'line':
         return (
-          <ResponsiveContainer width="100%" height={300}>
+          <ResponsiveContainer width="100%\" height={300}>
             <LineChart
               data={data}
               margin={{ top: 20, right: 30, left: 20, bottom: 10 }}
